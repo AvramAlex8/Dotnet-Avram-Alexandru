84ad7408b058594074791755d3041b3ab60f2a43
Avram <uiv00901@vitesco.com>
2021-11-02 19:53:03 +0200

    tutorial 11 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,13 +6,7 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
-            int a = -5;
-            Test(a);
-            Console.WriteLine(a);
-        }
-        static void Test(int x)
-        {
-            x = 100;
+            string x = "Caleb";
         }
     }
 }

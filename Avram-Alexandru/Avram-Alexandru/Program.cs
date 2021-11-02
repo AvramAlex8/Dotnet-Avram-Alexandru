80c9388049b36032c0a18efe6403c177d92d6c40
Avram <uiv00901@vitesco.com>
2021-11-02 21:30:23 +0200

    tutorial 17 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,12 +6,9 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
-            string name = "Caleb";
-            char[] characters = { 'H', 'e', 'l', 'l', 'o' };
-            string greeting = new string(characters);
-            Console.WriteLine(greeting);
-            string message = $"Hello, my name is {name} and I like to say {greeting}";
-            Console.WriteLine(message);
+            string name = "hello my name is Caleb!";
+            string[] words = name.Split(" ");
+            Console.WriteLine(words[1]);
         }
     }
 }

6666d556bc6336d2a633003b6a2a90a053e6c92d
Avram <uiv00901@vitesco.com>
2021-11-02 21:09:07 +0200

    tutorial 16 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,8 +6,12 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
-            char letter = 'r';
-            Console.WriteLine(letter);
+            string name = "Caleb";
+            char[] characters = { 'H', 'e', 'l', 'l', 'o' };
+            string greeting = new string(characters);
+            Console.WriteLine(greeting);
+            string message = $"Hello, my name is {name} and I like to say {greeting}";
+            Console.WriteLine(message);
         }
     }
 }

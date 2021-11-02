c1c0c74be2efac27bf504de8a82c2e672a62328c
Avram <uiv00901@vitesco.com>
2021-11-02 15:20:27 +0200

    project created

---
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avram_Alexandru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

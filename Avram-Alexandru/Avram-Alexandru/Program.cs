cdb30b4822e13c308c93b96e1fc2b7df210d5cdf
Avram <uiv00901@vitesco.com>
2021-11-02 20:48:39 +0200

    tutorial 15 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,9 +6,8 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
-            double x = -50.9;
-            double y = 300;
-            Console.WriteLine(Math.Sign(y));
+            char letter = 'r';
+            Console.WriteLine(letter);
         }
     }
 }

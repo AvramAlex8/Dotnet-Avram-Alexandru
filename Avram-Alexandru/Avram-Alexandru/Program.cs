d735cb6f2f5a0dfc90c4fff4d716e84819379726
Avram <uiv00901@vitesco.com>
2021-11-03 14:47:32 +0200

    tutorial 28 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,9 +6,17 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
+            bool logging = false;
             for (int i = 9; i >= 0; i -= 2)
             {
                 Console.WriteLine(i);
+                if(i == 7)
+                {
+                    if(logging == true)
+                    {
+                        Console.WriteLine("We found 7!");
+                    }
+                }
             }
         }
     }

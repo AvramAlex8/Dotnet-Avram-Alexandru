85e0b8131fe2bde0052cfa34ec12095005dea2df
Avram <uiv00901@vitesco.com>
2021-11-03 13:25:23 +0200

    tutorial 21 completed

--- Avram-Alexandru/Avram-Alexandru/Program.cs
+++ Avram-Alexandru/Avram-Alexandru/Program.cs
@@ -6,27 +6,13 @@ namespace Avram_Alexandru
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.FirstName = "Caleb";
-            person.LastName = "Curry";
-            Console.WriteLine("What do you think the name is?");
-            string fullNameGuess = Console.ReadLine();
-            if(person.FullName == fullNameGuess)
+            Console.WriteLine("What's your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What's the password?");
+            string password = Console.ReadLine();
+            if(!(age <= 12) || password != "password123")
             {
-                Console.WriteLine("You got the name! You win!");
-            }
-            else
-            {
-                Console.WriteLine("Second guess?");
-                fullNameGuess = Console.ReadLine();
-                if(person.FullName == fullNameGuess)
-                {
-                    Console.WriteLine("You got the name! You get 0.5 points!");
-                }
-                else
-                {
-                    Console.WriteLine("You're a dissapointment!");
-                }
+                Console.WriteLine("Welcome! You're atleast a teenager and got correct password.");
             }
         }
     }
